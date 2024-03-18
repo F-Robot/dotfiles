5f43bb488919a6aba5f329aca5f47de51b10aade
f-robot <ricardo.escobedo@afflux.mx>
2024-03-17 20:14:11 -0600

    refactor nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,99 @@
-vim.cmd([[source ~/.config/nvim/lua/autocmds.vim]])
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
-require('performance')
-require('settings')
-require('mappings')
-require('plugins')
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, lazy = false },
+  {
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'neovim/nvim-lspconfig',
+  },
+      {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    },
+  { 'nvim-lua/plenary.nvim' },
+
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+
+  {
+    'jay-babu/mason-null-ls.nvim',
+    event = { 'BufReadPre', 'BufNewFile' },
+    dependencies = {
+      'nvimtools/none-ls.nvim',
+    },
+  },
+})
+
+require('catppuccin').setup({
+  flavour = 'mocha',
+})
+
+vim.cmd([[colorscheme catppuccin]])
+
+require('mason').setup()
+require('mason-lspconfig').setup()
+
+
+local handlers = {
+  function(server_name) -- default handler (optional)
+    require('lspconfig')[server_name].setup({})
+  end,
+  ['lua_ls'] = function()
+    require('lspconfig').lua_ls.setup({
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' },
+          },
+        },
+      },
+    })
+  end,
+}
+require('mason-lspconfig').setup_handlers(handlers)
+require('mason-null-ls').setup({
+  handlers = {},
+})
+
+
+local keys =  {
+      { "<leader>cl", "<cmd>LspInfo<cr>", desc = "Lsp Info" },
+      { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end, desc = "Goto Definition", has = "definition" },
+      { "gr", "<cmd>Telescope lsp_references<cr>", desc = "References" },
+      { "gD", vim.lsp.buf.declaration, desc = "Goto Declaration" },
+      { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end, desc = "Goto Implementation" },
+      { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
+      { "K", vim.lsp.buf.hover, desc = "Hover" },
+      { "gK", vim.lsp.buf.signature_help, desc = "Signature Help", has = "signatureHelp" },
+      { "<c-k>", vim.lsp.buf.signature_help, mode = "i", desc = "Signature Help", has = "signatureHelp" },
+      { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "v" }, has = "codeAction" },
+      { "<leader>cc", vim.lsp.codelens.run, desc = "Run Codelens", mode = { "n", "v" }, has = "codeLens" },
+      { "<leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", mode = { "n" }, has = "codeLens" },
+      {
+        "<leader>cA",
+        function()
+          vim.lsp.buf.code_action({
+            context = {
+              only = {
+                "source",
+              },
+              diagnostics = {},
+            },
+          })
+        end,
+        desc = "Source Action",
+        has = "codeAction",
+      }
+    }
